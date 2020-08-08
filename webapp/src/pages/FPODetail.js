9b99e9d9f603bcae3eef6aa82ca150e116fc4d20
Tanisha <tanisha031199@gmail.com>
2020-08-08 20:23:10 +0530

    changes to search

---
+++ webapp/src/pages/FPODetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FPODetail() {
+    return (
+        <div>
+            <h1>FPO Organization</h1>
+            <h2>Details</h2>
+        </div>
+    )
+}
