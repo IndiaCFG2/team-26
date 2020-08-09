23c107334d40a714d598c0eb444b951fb66487dd
Tanisha <tanisha031199@gmail.com>
2020-08-09 09:27:01 +0530

    added registration

--- webapp/src/pages/Community.js
+++ webapp/src/pages/Community.js
@@ -7,6 +7,13 @@ import Navigation from '../components/Navigation';
 import Broadcast from "../components/broadcast"
 import ReactPlayer from "react-player";
 
+function Random() {
+    var maxNumber = 100;
+    var randomNumber = Math.floor((Math.random() * maxNumber) + 1);
+    return <span style={{marginLeft:"10px"}}>{randomNumber} likes</span>
+    // return <div>{randomNumber}</div>;
+  }
+
 function Home({data,status,getSlotTime}) {
     const [tag,setTag]=useState(0);
     const [topics,setTopics]=useState([]);
@@ -94,6 +101,7 @@ function Home({data,status,getSlotTime}) {
                                     <svg width="20" height="20" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M2 42H10V18H2V42ZM46 20C46 17.79 44.21 16 42 16H29.37L31.28 6.86C31.32 6.66 31.35 6.45 31.35 6.23C31.35 5.4 31.01 4.65 30.47 4.11L28.34 2L15.17 15.17C14.45 15.9 14 16.9 14 18V38C14 40.21 15.79 42 18 42H36C37.66 42 39.08 40.99 39.68 39.56L45.71 25.46C45.89 25 46 24.51 46 24V20.17L45.98 20.15L46 20Z" fill="black"/>
                                     </svg>
+                                    {/* <Random/> */}
                                     <span style={{marginLeft:"10px"}}>{5} likes</span>
                                 </div>
                             </div>
